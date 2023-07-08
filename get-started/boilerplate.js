6bc0cfcf10d9613f11bc615234722afacb7ca39a
parveen232 <parveen297m210@gmail.com>
2023-07-08 19:39:54 +0530

    mkdir sync version & writeFile

--- get-started/boilerplate.js
+++ get-started/boilerplate.js
@@ -1,7 +1,11 @@
 const fs = require("fs");
+const folderName =  process.argv[2] || 'Project'
 
-fs.mkdir('fruits', {recursive: true}, (err) => {
-    console.log("In the callback")
-    if(err) throw err;
-})
-console.log("i come after mkdir in file")
+try {
+    fs.mkdirSync(folderName)
+    fs.writeFileSync(`${folderName}/index.html`, '')
+    fs.writeFileSync(`${folderName}/styles.css`, '')
+    fs.writeFileSync(`${folderName}/app.js`, '')
+} catch (err) {
+    console.log('Error:' + err.message)
+}
