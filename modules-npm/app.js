e8baac113c4f1b1d61ab22f30d947dffb31d3389
parveen232 <parveen297m210@gmail.com>
2023-07-09 11:20:36 +0530

    module.exports

---
+++ modules-npm/app.js
@@ -0,0 +1,7 @@
+const math = require('./math')
+// const { square } = require('./math')
+// console.log(math)
+console.log(math.square(4))
+// console.log(square(4))
+
+// node app.js
---
+++ modules-npm/math.js
@@ -0,0 +1,8 @@
+const add = (x, y) => x + y;
+
+const square = x => x * x;
+// module.exports.square = x => x * x;
+
+// module.exports = "Hi there!"
+module.exports.add = add
+module.exports.square = square
