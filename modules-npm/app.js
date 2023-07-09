012606839bbc3cc09c528d47801abad06f79d3dc
parveen232 <parveen297m210@gmail.com>
2023-07-09 12:52:57 +0530

    requiring a directory

--- modules-npm/app.js
+++ modules-npm/app.js
@@ -1,7 +1,10 @@
 const math = require('./math')
+const fruits = require('./fruits')
 // const { square } = require('./math')
 // console.log(math)
 console.log(math.square(4))
 // console.log(square(4))
 
 // node app.js
+
+console.log(fruits)
---
+++ modules-npm/fruits/index.js
@@ -0,0 +1,7 @@
+const apple = require('./apple')
+const mango = require('./mango')
+const orange = require('./orange')
+
+const fruits = [apple, mango, orange]
+// console.log(fruits)
+module.exports = fruits;
